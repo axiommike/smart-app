9218338fdfb3cbd88737550bb6b397b50f20d046
Sean Goresht <seangores@gmail.com>
2015-04-30 15:55:21 -0600

    Allow disabling mortgage through yes-no on all mortgages Attempt a little aliasing via one-way binding on assets

--- app/components/property-asset.js
+++ app/components/property-asset.js
@@ -1,5 +1,6 @@
 import Ember from "ember";
 
 export default Ember.Component.extend({
+	hasMortgage: Ember.computed.oneWay("property.mortgage"),
 	property: null
 });
--- app/controllers/mortgage-application/assets.js
+++ app/controllers/mortgage-application/assets.js
@@ -8,49 +8,49 @@ export default Ember.ObjectController.extend({
 			this.send("addProperty");
 		}
 	}.observes("ownsOtherRealEstate"),
-	hasSavings: false,
+	hasSavings: Ember.computed.oneWay("model.applicant.savingsAssets.length"),
 	savingsToggled: function() {
 		if (this.get("hasSavings") && this.get("model.applicant.savingsAssets.length") === 0) {
 			this.send("addAsset", "savings");
 		}
 	}.observes("hasSavings"),
-	hasPersonalItems: false,
+	hasPersonalItems: Ember.computed.oneWay("model.applicant.personalItemAssets.length"),
 	personalItemsToggled: function() {
 		if (this.get("hasPersonalItems") && this.get("model.applicant.personalItemAssets.length") === 0) {
 			this.send("addAsset", "item");
 		}
 	}.observes("hasPersonalItems"),
-	hasGICs: false,
+	hasGICs: Ember.computed.oneWay("model.applicant.gicAssets.length"),
 	gicsToggled: function() {
 		if (this.get("hasGICs") && this.get("model.applicant.gicAssets.length") === 0) {
 			this.send("addAsset", "gic");
 		}
 	}.observes("hasGICs"),
-	hasRESPs: false,
+	hasRESPs: Ember.computed.oneWay("model.applicant.respAssets.length"),
 	respsToggled: function() {
 		if (this.get("hasRESPs") && this.get("model.applicant.respAssets.length") === 0) {
 			this.send("addAsset", "resp");
 		}
 	}.observes("hasRESPs"),
-	hasInvestments: false,
+	hasInvestments: Ember.computed.oneWay("model.applicant.investmentAssets.length"),
 	investmentsToggled: function() {
 		if (this.get("hasInvestments") && this.get("model.applicant.investmentAssets.length") === 0) {
 			this.send("addAsset", "investment");
 		}
 	}.observes("hasInvestments"),
-	hasVehicles: false,
+	hasVehicles: Ember.computed.oneWay("model.applicant.vehicles.length"),
 	vehiclesToggled: function() {
 		if (this.get("hasVehicles") && this.get("model.applicant.vehicles.length") === 0) {
 			this.send("addVehicle");
 		}
 	}.observes("hasVehicles"),
-	hasRRSPs: false,
+	hasRRSPs: Ember.computed.oneWay("model.applicant.rrspAssets.length"),
 	rrspsToggled: function() {
 		if (this.get("hasRRSPs") && this.get("model.applicant.rrspAssets.length") === 0) {
 			this.send("addAsset", "rrsp");
 		}
 	}.observes("hasRRSPs"),
-	ownsOtherAssets: false,
+	ownsOtherAssets: Ember.computed.oneWay("model.applicant.otherAssets.length"),
 	otherAssetsToggled: function() {
 		if (this.get("ownsOtherAssets") && this.get("model.applicant.otherAssets.length") === 0) {
 			this.send("addAsset", "other");
