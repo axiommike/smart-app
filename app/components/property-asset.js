e4d68aefc050aa08b1b2cba0ee0fe19c13069828
Sean Goresht <seangores@gmail.com>
2015-05-08 09:02:25 -0600

    Set line of credit to false by default

--- app/components/property-asset.js
+++ app/components/property-asset.js
@@ -3,5 +3,5 @@ import Ember from "ember";
 export default Ember.Component.extend({
 	hasMortgage: Ember.computed.oneWay("property.mortgage"),
 	property: null,
-	hasLineOfCredit: Ember.computed.oneWay("property.lineOfCredit")
+	hasLineOfCredit: false
 });
