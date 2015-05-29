6b558ef5bb4e8de38eaa1ec339f5ba4cef5eb865
Sean Goresht <seangores@gmail.com>
2015-05-29 14:19:03 -0600

    Show NOAS when self-employed Fix employment not removing

--- app/models/company.js
+++ app/models/company.js
@@ -3,7 +3,7 @@ import DS from "ember-data";
 export default DS.Model.extend({
 	name: DS.attr("string"),
 	description: DS.attr("string"),
-	address: DS.belongsTo("address", {async: true}),
+	address: DS.belongsTo("address"),
 	phone: DS.attr("string"),
 	fax: DS.attr("string")
 });
