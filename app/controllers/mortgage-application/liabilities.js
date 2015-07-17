ca0734059f2684437f597ba1dc4c560569c3891b
Sean Goresht <seangores@gmail.com>
2015-07-17 10:08:07 -0600

    Fix liabilities not showing up on save Fix saving of liabilities after advancing to co-applicants

--- app/controllers/mortgage-application/liabilities.js
+++ app/controllers/mortgage-application/liabilities.js
@@ -1,31 +1,31 @@
 import Ember from "ember";
 
 export default Ember.Controller.extend({
-	hasCreditCard: false,
+	hasCreditCard: Ember.computed.oneWay("model.applicant.creditCardLiabilities.length"),
 	creditCardToggled: function() {
 		if (this.get("hasCreditCard") && this.get("model.applicant.creditCardLiabilities.length") === 0) {
 			this.send("addLiability", "credit-card");
 		}
 	}.observes("hasCreditCard"),
-	hasLineOfCredit: false,
+	hasLineOfCredit: Ember.computed.oneWay("model.applicant.lineOfCreditLiabilities.length"),
 	lineOfCreditToggled: function() {
 		if (this.get("hasLineOfCredit") && this.get("model.applicant.lineOfCreditLiabilities.length") === 0) {
 			this.send("addLiability", "line-of-credit");
 		}
 	}.observes("hasLineOfCredit"),
-	hasLoans: false,
+	hasLoans: Ember.computed.oneWay("model.applicant.loanLiabilities.length"),
 	loansToggled: function() {
 		if (this.get("hasLoans") && this.get("model.applicant.loanLiabilities.length") === 0) {
 			this.send("addLiability", "loan");
 		}
 	}.observes("hasLoans"),
-	hasChildSupport: false,
+	hasChildSupport: Ember.computed.oneWay("model.applicant.childSupportLiabilities.length"),
 	childSupportToggled: function() {
 		if (this.get("hasChildSupport") && this.get("model.applicant.childSupportLiabilities.length") === 0) {
 			this.send("addLiability", "child-support");
 		}
 	}.observes("hasChildSupport"),
-	hasOtherLiabilities: false,
+	hasOtherLiabilities: Ember.computed.oneWay("model.applicant.otherLiabilities.length"),
 	otherLiabilitiesToggled: function() {
 		if (this.get("hasOtherLiabilities") && this.get("model.applicant.otherLiabilities.length") === 0) {
 			this.send("addLiability", "other");
@@ -39,8 +39,11 @@ export default Ember.Controller.extend({
 			this.send("removeLiabilityMaster", liability);
 		},
 		nextStep: function() {
-			this.get("model").save().then((application) => {
-				this.transitionToRoute("mortgage-application.summary", application);
+			this.send("saveLiabilities");
+			this.get("model.applicant").save().then((savedApplicant) => {
+				this.get("model").save().then((application) => {
+					this.transitionToRoute("mortgage-application.summary", application);
+				});
 			});
 		}
 	}
