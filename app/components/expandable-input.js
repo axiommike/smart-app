57548d9a506a50665824e21df998019c8a6f297b
Sean <seangores@gmail.com>
2015-09-30 15:24:06 -0600

    Fix no remove button for extra income sources Index, applicant card: allow automatically-expanding inputs to accomodate for longer e-mails and names

---
+++ app/components/expandable-input.js
@@ -0,0 +1,9 @@
+import Ember from "ember";
+
+export default Ember.TextField.extend({
+	type: "text",
+	defaultSize: 20,
+	size: Ember.computed("value", function() {
+		return this.get("value.length") > 0 ? this.get("value.length") : this.get("defaultSize");
+	})
+});
