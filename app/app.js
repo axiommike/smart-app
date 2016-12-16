0ceca975986abbd320530d764081ea77c14d46c4
Robert Wagner <rwwagner90@gmail.com>
2016-12-15 19:29:59 -0500

    Fix application import

--- app/app.js
+++ app/app.js
@@ -2,13 +2,13 @@ import Ember from 'ember';
 import Resolver from './resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
-const { Application: { EmberApplication } } = Ember;
+const { Application } = Ember;
 
 let App;
 
 Ember.MODEL_FACTORY_INJECTIONS = true;
 
-App = EmberApplication.extend({
+App = Application.extend({
   modulePrefix: config.modulePrefix,
   podModulePrefix: config.podModulePrefix,
   Resolver
