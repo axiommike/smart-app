7f3b533a7c12c0b5e8351e0f79fe5e6c79d029e3
Robert Wagner <rwwagner90@gmail.com>
2016-12-17 15:00:16 -0500

    Tweak income monthly/yearly stuff
    I was not actually able to reproduce an error where I could not remove
    the extra income sources. I did find other errors with the feature
    though, and I have fixed them here.
    
    I removed `monthly_value` and `yearly_value` in favor of just using the
    normal `value` field, and I added a `frequency` field.
    
    Do we want to do this approach on the API side @ericmaicon?
    
    This PR is related to #9

--- app/models/income.js
+++ app/models/income.js
@@ -2,10 +2,9 @@ import Model from 'ember-data/model';
 import attr from 'ember-data/attr';
 
 export default Model.extend({
+  frequency: attr('string', {defaultValue: 'yearly'}),
   income_source: attr('string'),
   pension_source: attr('string'),
   person_id: attr('string'),
-  value: attr('integer'),
-  monthly_value: attr('integer'),
-  yearly_value: attr('integer')
+  value: attr('number')
 });
