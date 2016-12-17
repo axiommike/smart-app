3c29185b759a0371f798bb0d675fb9c5e06192d1
Robert Wagner <rwwagner90@gmail.com>
2016-12-17 11:47:22 -0500

    Fix formatting currency on first page
    Resolves #14

--- app/models/applicant.js
+++ app/models/applicant.js
@@ -3,25 +3,26 @@ import attr from 'ember-data/attr';
 import { belongsTo, hasMany } from 'ember-data/relationships';
 
 export default Model.extend({
+  agent_id: attr('string'),
+  birth_date: attr('string'),
+  comment: attr('string'),
+  down_payment: attr('string'),
+  down_payment_explanation: attr('string'),
+  down_payment_source: attr('string'),
+  email: attr('string'),
   first_name: attr('string'),
   last_name: attr('string'),
-  email: attr('string'),
+  marital_status: attr('string'),
   home_phone: attr('string'),
   referral_source: attr('string'),
-  referred_friend: attr('string'),
   referred_agent: attr('string'),
+  referred_friend: attr('string'),
   referred_other: attr('string'),
-  type: attr('string'),
-  down_payment: attr('string'),
-  down_payment_source: attr('string'),
-  down_payment_explanation: attr('string'),
-  comment: attr('string'),
-  agent_id: attr('string'),
   relationship: attr('string'),
-  birth_date: attr('string'),
-  marital_status: attr('string'),
-  agent: belongsTo('agent'),
+  type: attr('string'),
+
   addresses: hasMany('address', { async: true }),
+  agent: belongsTo('agent'),
   employments: hasMany('employment', { async: true }),
   incomes: hasMany('income', { async: true })
 });
