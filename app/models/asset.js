9ad07fd6c5b49c025358c134f36e8495df1831a0
Robert Wagner <rwwagner90@gmail.com>
2016-12-18 11:40:34 -0500

    Change assetProxy to asset, refactor template, add back asset model

--- app/models/asset.js
+++ app/models/asset.js
@@ -1,4 +1,25 @@
+import Ember from 'ember';
 import Model from 'ember-data/model';
+import attr from 'ember-data/attr';
+import { belongsTo } from 'ember-data/relationships';
+const { computed } = Ember;
 
 export default Model.extend({
+  assetType: attr("string"),
+  bank: attr("string"),
+  description: attr("string"), /* Primarily used for "other" type */
+  ownership: attr("string"),
+  value: attr("number", { defaultValue: 0 }),
+  valueType: attr("string"),
+
+  applicant: belongsTo("applicant"),
+
+  isPersonalItem: computed.equal("type", "item"),
+  isSavings: computed.equal("type", "savings"),
+  isInvestment: computed.equal("type", "investment"),
+  isProperty: computed.equal("type", "property"),
+  isGIC: computed.equal("type", "gic"),
+  isRRSP: computed.equal("type", "rrsp"),
+  isVehicle: computed.equal("type", "vehicle"),
+  isOther: computed.equal("type", "other"),
 });
