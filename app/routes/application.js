97e434eb5edf62e41f3d2d6665aedd7ee275fdea
Robert Wagner <rwwagner90@gmail.com>
2016-12-18 01:12:08 -0500

    Grab the first applicant in a mortgage
    Resolves #22

--- app/routes/application.js
+++ app/routes/application.js
@@ -4,7 +4,11 @@ const { Route } = Ember;
 export default Route.extend({
   actions: {
     sendIncomplete(model) {
-      model.save().then((applicant) => {
+      model.save().then((model) => {
+        let applicant = model;
+        if(model.constructor.modelName === 'mortgage'){
+          applicant = model.get('applicants').toArray()[0];
+        }
         this.transitionTo('thank-you', { queryParams: { id: applicant.get('id'), is_incomplete: true } });
       });
     }
